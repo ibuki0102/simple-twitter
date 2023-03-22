78fd51c8937c31e8ba83fa89ceba490b9bd7a169
ibuki <bigfrog0102@gmail.com>
2023-03-23 01:10:57 +0800

    Using HashRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
   UserFollowPage,
   OtherTweetPage,
 } from 'pages'
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import styles from 'App.module.scss'
 import { userData } from 'assets/userData'
 import { replyTweetModalData } from 'assets/replyTweetModalData'
@@ -22,30 +22,35 @@ import ProfileEditModal from 'components/ProfileEditModal/ProfileEditModal'
 function App() {
   return (
     <div className={styles.App}>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="regist" element={<RegistPage />} />
-        <Route path="admin" element={<AdminLoginPage />} />
-        <Route path="admin/tweetList" element={<AdminTweetListPage />} />
-        <Route path="admin/userList" element={<AdminUserListPage />} />
-        <Route path="main" element={<MainPage />} />
-        <Route path="main/replyList" element={<ReplyListPage />} />
-        <Route path="/user/self" element={<UserTweetPage />} />
-        <Route path="/user/follow" element={<UserFollowPage />} />
-        <Route path="/user/other" element={<OtherTweetPage />} />
-        <Route path="setting" element={<SettingPage userData={userData} />} />
-        {/* 雪央註: Modal的路由僅供顯示，之後會在MainPage以及ReplyListPage裡用Component使用 */}
-        <Route path="tweetModal" element={<TweetModal userData={userData} />} />
-        <Route
-          path="replyModal"
-          element={<ReplyModal replyTweetModalData={replyTweetModalData} />}
-        />
-        <Route
-          path="profileEditModal"
-          element={<ProfileEditModal userData={userData} />}
-        />
-        <Route path="*" element={<LoginPage />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="regist" element={<RegistPage />} />
+          <Route path="admin" element={<AdminLoginPage />} />
+          <Route path="admin/tweetList" element={<AdminTweetListPage />} />
+          <Route path="admin/userList" element={<AdminUserListPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path="main/replyList" element={<ReplyListPage />} />
+          <Route path="/user/self" element={<UserTweetPage />} />
+          <Route path="/user/follow" element={<UserFollowPage />} />
+          <Route path="/user/other" element={<OtherTweetPage />} />
+          <Route path="setting" element={<SettingPage userData={userData} />} />
+          {/* 雪央註: Modal的路由僅供顯示，之後會在MainPage以及ReplyListPage裡用Component使用 */}
+          <Route
+            path="tweetModal"
+            element={<TweetModal userData={userData} />}
+          />
+          <Route
+            path="replyModal"
+            element={<ReplyModal replyTweetModalData={replyTweetModalData} />}
+          />
+          <Route
+            path="profileEditModal"
+            element={<ProfileEditModal userData={userData} />}
+          />
+          <Route path="*" element={<LoginPage />} />
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
-
-const basename = process.env.PUBLIC_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter basename={basename}>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 )
