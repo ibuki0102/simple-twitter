32da84b07065b181e98ebee99484b5db364b3a0f
ibuki <bigfrog0102@gmail.com>
2023-03-22 23:02:07 +0800

    feat: use BrowserRouter at index.js instead

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
   ReplyListPage,
   UserTweetPage,
 } from 'pages'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import styles from 'App.module.scss'
 import { userData } from 'assets/userData'
 import { replyTweetModalData } from 'assets/replyTweetModalData'
@@ -17,38 +17,31 @@ import TweetModal from 'components/TweetModal/TweetModal'
 import ReplyModal from 'components/ReplyModal/ReplyModal'
 import ProfileEditModal from 'components/ProfileEditModal/ProfileEditModal'
 
-const basename = process.env.PUBLIC_URL
-
 function App() {
   return (
     <div className={styles.App}>
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="regist" element={<RegistPage />} />
-          <Route path="admin" element={<AdminLoginPage />} />
-          <Route path="admin/tweetList" element={<AdminTweetListPage />} />
-          <Route path="admin/userList" element={<AdminUserListPage />} />
-          <Route path="main" element={<MainPage />} />
-          <Route path="main/replyList" element={<ReplyListPage />} />
-          <Route path="/user/self" element={<UserTweetPage />} />
-          <Route path="setting" element={<SettingPage userData={userData} />} />
-          {/* 雪央註: Modal的路由僅供顯示，之後會在MainPage以及ReplyListPage裡用Component使用 */}
-          <Route
-            path="tweetModal"
-            element={<TweetModal userData={userData} />}
-          />
-          <Route
-            path="replyModal"
-            element={<ReplyModal replyTweetModalData={replyTweetModalData} />}
-          />
-          <Route
-            path="profileEditModal"
-            element={<ProfileEditModal userData={userData} />}
-          />
-          <Route path="*" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="regist" element={<RegistPage />} />
+        <Route path="admin" element={<AdminLoginPage />} />
+        <Route path="admin/tweetList" element={<AdminTweetListPage />} />
+        <Route path="admin/userList" element={<AdminUserListPage />} />
+        <Route path="main" element={<MainPage />} />
+        <Route path="main/replyList" element={<ReplyListPage />} />
+        <Route path="/user/self" element={<UserTweetPage />} />
+        <Route path="setting" element={<SettingPage userData={userData} />} />
+        {/* 雪央註: Modal的路由僅供顯示，之後會在MainPage以及ReplyListPage裡用Component使用 */}
+        <Route path="tweetModal" element={<TweetModal userData={userData} />} />
+        <Route
+          path="replyModal"
+          element={<ReplyModal replyTweetModalData={replyTweetModalData} />}
+        />
+        <Route
+          path="profileEditModal"
+          element={<ProfileEditModal userData={userData} />}
+        />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
+const basename = process.env.PUBLIC_URL
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
