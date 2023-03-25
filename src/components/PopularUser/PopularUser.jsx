ff46ee5978613dc0b302641fef7cbcb968fb8c71
ibuki <bigfrog0102@gmail.com>
2023-03-26 00:20:09 +0800

    feat: popular list now have api return data

--- src/components/PopularUser/PopularUser.jsx
+++ src/components/PopularUser/PopularUser.jsx
@@ -1,17 +1,54 @@
 // Jasmine
 
 import styles from 'components/PopularUser/PopularUser.module.scss'
-import { ReactComponent as DefaultAvatar } from 'assets/icons/default_avatar.svg'
+import { followUser, unFollowUser } from 'api/follow'
+import { useState } from 'react'
 
-const PopularUser = ({ userData }) => {
+const PopularUser = ({ userData, updateTweetList, setUpdateTweetList }) => {
+  const [isFollowed, setIsFollow] = useState(userData.isFollowed)
+  const handleClickFollow = async () => {
+    const token = localStorage.getItem('token')
+    const id = userData.id
+    const data = await followUser({ token, id })
+    if (data) {
+      setIsFollow(true)
+      setUpdateTweetList(!updateTweetList)
+    }
+  }
+  const handleClickUnFollow = async () => {
+    const token = localStorage.getItem('token')
+    const id = userData.id
+    const data = await unFollowUser({ token, id })
+    if (data) {
+      setIsFollow(false)
+      setUpdateTweetList(!updateTweetList)
+    }
+  }
   return (
     <div className={styles.PopularUserContainer}>
-      <DefaultAvatar className={styles.DefaultAvatar} />
-      <div className={styles.PopularUserName}>
-        <div className={styles.UserName}>Pizza Hut</div>
-        <div className={styles.UserAcount}>@pizzahut</div>
+      <div className={styles.UserInfoContainer}>
+        <img
+          src={userData.avatar}
+          className={styles.DefaultAvatar}
+          alt="avatar"
+        />
+        <div className={styles.PopularUserName}>
+          <div className={styles.UserName}>{userData.name || 'UserName'}</div>
+          <div className={styles.UserAcount}>
+            @{userData.account || 'UserAccount'}
+          </div>
+        </div>
       </div>
-      <button>正在跟隨</button>
+
+      {isFollowed ? (
+        <button className={styles.ActiveButton} onClick={handleClickUnFollow}>
+          正在跟隨
+        </button>
+      ) : (
+        <button className={styles.DefaultButton} onClick={handleClickFollow}>
+          跟隨
+        </button>
+      )}
     </div>
   )
 }
