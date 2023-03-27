4be3b976d797e94f00ccec7141a44504cf98eb36
chenchiahsiu <hebe12311@gmail.com>
2023-03-27 13:16:38 +0800

    feat: connect API of AdminTweetList

---
+++ src/api/admin.js
@@ -0,0 +1,20 @@
+// Jasmine
+
+import axios from 'axios'
+
+const authURL = 'https://enigmatic-refuge-29514.herokuapp.com/api'
+
+// Jasmine 新增: 拿到後台推文清單的 API
+export const getAdminTweetList = async ({ token }) => {
+  try {
+    const { data } = await axios.get(`${authURL}/admin/tweets`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+    return data
+  } catch (error) {
+    console.error('[GetUserData Failed]:', error)
+    return { error }
+  }
+}
--- src/pages/AdminTweetListPage/AdminTweetListPage.jsx
+++ src/pages/AdminTweetListPage/AdminTweetListPage.jsx
@@ -3,12 +3,36 @@
 import Sidebar from 'components/Sidebar/Sidebar'
 import styles from '../AdminTweetListPage/AdminTweetListPage.module.scss'
 import AdminTweetListItem from 'components/AdminTweetListItem/AdminTweetListItem'
-import { AdminTweetListData } from 'assets/adminTweetListData'
+import { getAdminTweetList } from 'api/admin'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AdminTweetListPage = () => {
-  const tweetlistItems = AdminTweetListData.map((listData) => (
-    <AdminTweetListItem listData={listData} />
-  ))
+  const navigate = useNavigate()
+
+  // Jasmine 註: 管理後台推文
+  const [adminTweets, setAdminTweets] = useState([])
+
+  // // Jasmine 註: 取得後台推文
+  useEffect(() => {
+    const getAdminTweets = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        const userId = localStorage.getItem('userId')
+        if (!token) {
+          navigate('/admin')
+        }
+        const adminTweets = await getAdminTweetList({ token, userId })
+        if (adminTweets) {
+          setAdminTweets(adminTweets)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getAdminTweets()
+  }, [navigate])
+
   return (
     <div className={styles.Container}>
       <div className={styles.Sidebar}>
@@ -16,7 +40,13 @@ const AdminTweetListPage = () => {
       </div>
       <div className={styles.TweetListContainer}>
         <div className={styles.Header}>推文清單</div>
-        <div className={styles.ListItemContainer}>{tweetlistItems}</div>
+        <div className={styles.ListItemContainer}>
+          {adminTweets.map((adminTweet) => {
+            return (
+              <AdminTweetListItem key={adminTweet.id} adminTweet={adminTweet} />
+            )
+          })}
+        </div>
       </div>
     </div>
   )
